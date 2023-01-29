6079f4c1d72ea89d102582f8e3f7a9b8984631ef
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2023-01-28 20:28:24 -0500

    Card page

---
+++ pages/card/[card].tsx
@@ -0,0 +1,50 @@
+import Page from 'components/page';
+import { AllCards, CardName } from 'lib/constants';
+
+import type {
+	GetStaticPathsResult,
+	GetStaticPropsContext,
+	GetStaticPropsResult,
+} from 'next';
+
+type CardPageContext = {
+	card: CardName;
+};
+
+interface CardPageProps {
+	card: {
+		name: string;
+		suit?: 'swords' | 'pentacles' | 'cups' | 'wands';
+	};
+}
+
+export default function CardPage({ card }: CardPageProps) {
+	return <Page title={card?.name}>{card?.name ?? 'Loading...'}</Page>;
+}
+
+export async function getStaticProps(
+	context: GetStaticPropsContext<CardPageContext>
+): Promise<GetStaticPropsResult<CardPageProps>> {
+	const { card } = context.params ?? {};
+	if (!card) {
+		return {
+			notFound: true,
+		};
+	}
+	return {
+		props: {
+			card: {
+				name: card,
+			},
+		},
+	};
+}
+
+export async function getStaticPaths(): Promise<
+	GetStaticPathsResult<CardPageContext>
+> {
+	return {
+		paths: AllCards.map((card) => ({ params: { card } })),
+		fallback: true,
+	};
+}
