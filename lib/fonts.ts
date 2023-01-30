47e45e7ce2da403a711c77c088ec22c49cec03ca
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2023-01-29 19:34:39 -0500

    Fixes fonts

---
+++ lib/fonts.ts
@@ -0,0 +1,10 @@
+import { Merienda, Playfair_Display } from '@next/font/google';
+
+export const merienda = Merienda({
+	subsets: ['latin'],
+	variable: '--font-header',
+});
+export const playfair = Playfair_Display({
+	subsets: ['latin'],
+	variable: '--font-body',
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import { Merienda, Playfair_Display } from '@next/font/google';
+import { merienda, playfair } from 'lib/fonts';
 
 import type { AppProps } from 'next/app';
 
 import 'styles/globals.css';
-
-const merienda = Merienda({ subsets: ['latin'] });
-const playfair = Playfair_Display({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
