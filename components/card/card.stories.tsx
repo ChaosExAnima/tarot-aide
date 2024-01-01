6dd9a1a8b993c0c80b50700779a28cb3950a97bc
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-01 17:15:33 -0500

    Add animated linked card component

--- components/card/card.stories.tsx
+++ components/card/card.stories.tsx
@@ -5,17 +5,24 @@ import type { Meta, StoryObj } from '@storybook/react';
 export default {
 	title: 'Card',
 	component: Card,
+	parameters: {
+		controls: {
+			exclude: ['card'],
+		},
+	},
 } satisfies Meta<typeof Card>;
 type Story = StoryObj<typeof Card>;
 
 export const Primary: Story = {
 	args: {
-		link: false,
 		card: {
 			name: 'Test Card',
 			image: 'https://picsum.photos/seed/storybook/300/550.webp?blur',
 		},
 		width: 300,
 		height: 550,
+		visible: true,
+		link: false,
+		follow: false,
 	},
 };
--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,4 +1,6 @@
+import { animated, useSpring, config as springConfig } from '@react-spring/web';
 import Image from 'next/image';
+import { useCallback, useEffect, useState } from 'react';
 
 import { displayCase } from 'lib/text';
 
@@ -10,15 +12,60 @@ export interface CardProps {
 	card: GenericCard;
 	width?: number;
 	height?: number;
+	visible?: boolean;
+	follow?: boolean;
 }
+
+const CONSTRAINT = 100;
 
 export default function PlainCard({
 	card,
 	width = 300,
 	height = 550,
+	visible = true,
+	follow = false,
 }: CardProps) {
+	const [{ xys, opacity }, api] = useSpring(() => ({
+		xys: [0, 0, 1],
+		opacity: visible ? 1 : 0,
+		config: (key) =>
+			key === 'xys' ? springConfig.slow : springConfig.stiff,
+	}));
+
+	// Follow the mouse
+	// Maybe break out via https://github.com/pmndrs/react-spring/tree/master/demo/src/sandboxes/macos-dock/src
+	const move = useCallback(
+		({ clientX, clientY }: MouseEvent) =>
+			visible && api.set({ xys: mouseLook(clientX, clientY) }),
+		[api, visible]
+	);
+	useEffect(() => {
+		if (!follow) {
+			window.removeEventListener('mousemove', move);
+			return;
+		}
+		window.addEventListener('mousemove', move);
+		return () => window.removeEventListener('mousemove', move);
+	}, [follow, move]);
+
+	const [prevVisible, setPrevVisible] = useState(visible);
+	if (prevVisible !== visible) {
+		setPrevVisible(visible);
+
+		api.start({
+			opacity: visible ? 1 : 0,
+			xys: [visible ? 0 : 20, 0, visible ? 1 : 0.5],
+		});
+	}
+
 	return (
-		<figure className={classes.card}>
+		<animated.figure
+			style={{
+				transform: xys.to(transform),
+				opacity,
+			}}
+			className={classes.card}
+		>
 			{card.image && (
 				<Image
 					src={card.image}
@@ -31,6 +78,17 @@ export default function PlainCard({
 			<figcaption className={classes.caption}>
 				<span className={classes.name}>{displayCase(card.name)}</span>
 			</figcaption>
-		</figure>
+		</animated.figure>
 	);
+}
+
+function transform(x: number, y: number, s: number) {
+	return `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+}
+function mouseLook(mouseX: number, mouseY: number, zoom = 1) {
+	return [
+		-(mouseY - window.innerHeight / 2) / CONSTRAINT,
+		(mouseX - window.innerWidth / 2) / CONSTRAINT,
+		zoom,
+	];
 }
