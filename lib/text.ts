36bce25c1c702826926291cf6fc48684f2d8cca4
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-03 20:02:32 -0500

    Refactor displayCase function to handle small words

--- lib/text.ts
+++ lib/text.ts
@@ -1,7 +1,8 @@
 export function displayCase(input: string): string {
-	return input.replace(
-		/(\w)(\w*)/g,
-		(_, firstChar: string, rest: string) =>
-			firstChar.toUpperCase() + rest.toLowerCase()
-	);
+	return input.replace(/(\w)(\w*)/g, (_, firstChar: string, rest: string) => {
+		if (rest.length < 2) {
+			return firstChar + rest;
+		}
+		return firstChar.toUpperCase() + rest;
+	});
 }
