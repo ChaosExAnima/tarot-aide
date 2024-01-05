74bb53e178f579cc0a7514fb6f0b6cd9e746d573
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-05 17:29:19 -0500

    Add SpreadResponseBody interface and update spread API endpoint

--- pages/api/spread.ts
+++ pages/api/spread.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { isCard } from 'lib/cards/utils';
 import prisma from 'lib/db';
 import { processPhoto } from 'lib/media';
+import { SpreadResponseBody } from 'lib/spreads/api';
 import { getCurrentUserId } from 'lib/users';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -40,10 +41,10 @@ export const config = {
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse,
+	res: NextApiResponse<SpreadResponseBody>,
 ) {
 	if (req.method !== 'POST') {
-		res.status(405).json({ message: 'Method not allowed' });
+		res.status(405).json({ success: false, message: 'Method not allowed' });
 		return;
 	}
 
@@ -72,10 +73,14 @@ export default async function handler(
 			await processPhoto(files.photo[0], spread.id);
 		}
 
-		res.redirect(`/spreads/${spread.id}`);
+		res.status(200).json({
+			success: true,
+			message: 'Spread created',
+			spreadId: spread.id,
+		});
 	} catch (error) {
 		console.log('Invalid request:', error);
-		res.status(400).json({ message: 'Invalid request' });
+		res.status(400).json({ success: false, message: 'Invalid request' });
 		return;
 	}
 }
