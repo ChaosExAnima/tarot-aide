df33ec8c8db5608bf6323ce09a57c376cd9ec222
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-05 18:45:05 -0500

    Add displayRelativeDate function to lib/text.ts

--- lib/text.ts
+++ lib/text.ts
@@ -6,3 +6,21 @@ export function displayCase(input: string): string {
 		return firstChar.toUpperCase() + rest;
 	});
 }
+
+const rtf = new Intl.RelativeTimeFormat('en', {
+	localeMatcher: 'best fit',
+	numeric: 'always', // other values: "auto"
+	style: 'short',
+});
+
+export function displayRelativeDate(date: Date): string {
+	const diff = date.getTime() - Date.now();
+	const days = diff / (1000 * 60 * 60 * 24);
+	if (days < 0 && days > -1) {
+		return 'earlier today';
+	}
+	if (days > -1 && days < -2) {
+		return 'yesterday';
+	}
+	return rtf.format(days, 'day');
+}
