74164607d7f2e25ef06ad76bdd1cff4d70af5f1a
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-05 16:57:39 -0500

    Add users.ts file and update package.json and .eslintrc.json

---
+++ lib/users.ts
@@ -0,0 +1,3 @@
+export function getCurrentUserId(): number {
+	return 1; // TODO: get from db or headers
+}
--- pages/api/spread.ts
+++ pages/api/spread.ts
@@ -1,14 +1,35 @@
 import { Formidable } from 'formidable';
 import { z } from 'zod';
 
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { isCard } from 'lib/cards/utils';
+import prisma from 'lib/db';
+import { processPhoto } from 'lib/media';
+import { getCurrentUserId } from 'lib/users';
 
+import type { NextApiRequest, NextApiResponse } from 'next';
 const bodySchema = z.object({
-	date: z.coerce.date().optional(),
-	photo: z.instanceof(File).optional(),
-	cards: z.array(z.string()).nonempty(),
+	date: z.coerce
+		.date()
+		.refine((date) => date < new Date(), {
+			message: 'Cannot create spreads in the future',
+		})
+		.default(() => new Date()),
+	cards: z
+		.array(
+			z.string().refine((card) => isCard(card), {
+				message: 'Invalid card name',
+			}),
+		)
+		.nonempty(),
 	name: z.string().optional(),
 	description: z.string().optional(),
+});
+
+const form = new Formidable({
+	uploadDir: 'uploads',
+	keepExtensions: true,
+	allowEmptyFiles: false,
+	maxFiles: 1,
 });
 
 export const config = {
@@ -27,18 +48,34 @@ export default async function handler(
 	}
 
 	try {
-		const form = new Formidable({
-			uploadDir: './',
-			keepExtensions: true,
-		});
 		const [fields, files] = await form.parse(req);
-		const spread = bodySchema.parse(fields);
-		console.log(files, spread);
-		res.status(200).json({ message: 'Spread saved', spread });
-	} catch (error) {
-		console.log(error);
+		const { cards, ...spreadBody } = bodySchema.parse(fields);
 
-		res.status(400).json({ message: 'Invalid request', error });
+		const spread = await prisma.spread.create({
+			data: {
+				userId: getCurrentUserId(),
+				...spreadBody,
+			},
+		});
+
+		for (const card of cards) {
+			await prisma.position.create({
+				data: {
+					name: '',
+					spreadId: spread.id,
+					card,
+				},
+			});
+		}
+
+		if (files.photo?.length) {
+			await processPhoto(files.photo[0], spread.id);
+		}
+
+		res.redirect(`/spreads/${spread.id}`);
+	} catch (error) {
+		console.log('Invalid request:', error);
+		res.status(400).json({ message: 'Invalid request' });
 		return;
 	}
 }
