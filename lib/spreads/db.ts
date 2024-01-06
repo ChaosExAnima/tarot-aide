64d65b8d5146fc269a8991f9572224d3ead33f51
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-05 21:44:24 -0500

    Add getSpreadsForUser function and update SpreadsPage component

--- lib/spreads/db.ts
+++ lib/spreads/db.ts
@@ -1,12 +1,26 @@
+import { Prisma } from '@prisma/client';
+
 import prisma from 'lib/db';
 import { isAudio, isPhoto } from 'lib/media';
-import { getCurrentUserId } from 'lib/users';
 
 import { ExistingSpread } from './types';
 
+export async function getSpreadsForUser(
+	userId: number,
+): Promise<ExistingSpread[]> {
+	const spreads = await prisma.spread.findMany({
+		where: { userId },
+		include: {
+			positions: true,
+			media: true,
+		},
+	});
+	return spreads.map(dbToExistingSpread);
+}
+
 export async function getSpreadById(
 	id: number,
-	userId = getCurrentUserId(),
+	userId: number,
 ): Promise<ExistingSpread | null> {
 	const spread = await prisma.spread.findFirst({
 		where: { id, userId },
@@ -18,6 +32,14 @@ export async function getSpreadById(
 	if (!spread) {
 		return null;
 	}
+	return dbToExistingSpread(spread);
+}
+
+function dbToExistingSpread(
+	spread: Prisma.SpreadGetPayload<{
+		include: { positions: true; media: true };
+	}>,
+): ExistingSpread {
 	return {
 		...spread,
 		notes: spread.note,
