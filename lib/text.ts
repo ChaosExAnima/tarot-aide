f00c7c54eced5c3f9e2940c7933be4069cda7737
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-07 01:02:41 -0500

    Add displayCase, displayDate, displayRelativeDate, and displayOrdinal functions

--- lib/text.ts
+++ lib/text.ts
@@ -1,3 +1,9 @@
+/**
+ * Converts the first character of each word in a string to uppercase.
+ *
+ * @param input - The input string to be converted.
+ * @returns The converted string with the first character of each word in uppercase.
+ */
 export function displayCase(input: string): string {
 	return input.replace(
 		/([a-z])(\w{2,})/g,
@@ -5,6 +11,12 @@ export function displayCase(input: string): string {
 	);
 }
 
+/**
+ * Converts a date object to a formatted string representation.
+ *
+ * @param date - The date object to be formatted.
+ * @returns The formatted string representation of the date.
+ */
 export function displayDate(date: Date): string {
 	return date.toLocaleDateString('en', {
 		year: 'numeric',
@@ -19,20 +31,41 @@ const rtf = new Intl.RelativeTimeFormat('en', {
 	style: 'short',
 });
 
+/**
+ * Display a relative date, e.g. "today", "yesterday", "2 days ago", "3 months ago", "1 year ago"
+ *
+ * @param date - The date to display.
+ * @returns A string representing the relative date.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat
+ */
 export function displayRelativeDate(date: Date): string {
 	const diff = date.getTime() - Date.now();
-	const days = diff / (1000 * 60 * 60 * 24);
-	if (days < 0 && days > -1) {
-		return 'earlier today';
-	}
-	if (days > -1 && days < -2) {
+	const days = Math.ceil(diff / (1000 * 60 * 60 * 24)) * -1;
+
+	if (days === 0) {
+		return 'today';
+	} else if (days === 1) {
 		return 'yesterday';
+	} else if (days <= 30) {
+		return rtf.format(days, 'day');
+	} else if (days < 365) {
+		const months = Math.floor(days / 30);
+		return rtf.format(months, 'month');
+	} else if (days >= 365) {
+		const years = Math.floor(days / 365);
+		return rtf.format(years, 'year');
 	}
 	return rtf.format(days, 'day');
 }
 
 const ordinalFormatter = new Intl.PluralRules('en', { type: 'ordinal' });
 
+/**
+ * Converts a number to its ordinal representation.
+ *
+ * @param value - The number to be converted.
+ * @returns The ordinal representation of the number.
+ */
 export function displayOrdinal(value: number): string {
 	switch (ordinalFormatter.select(value)) {
 		case 'one':
