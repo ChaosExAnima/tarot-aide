e390fd5c448d4bdcf8f000a323fd927b7886bdf5
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-08 16:19:41 -0500

    Refactor getCardReferences function to simplify code

--- lib/cards/db.ts
+++ lib/cards/db.ts
@@ -8,25 +8,10 @@ import { isTarotCard } from './utils';
 import type { Prisma } from '@prisma/client';
 
 export async function getCardReferences(card: GenericCard, reversed = false) {
-	const references: CardReference[] = [];
-	if (!card.id) {
-		const dbCard = await prisma.card.findFirst({
-			where: { name: card.name, userId: getCurrentUserId() },
-			include: {
-				references: {
-					where: { reversed },
-				},
-			},
-		});
-		if (dbCard) {
-			references.push(...dbCard.references.map(dbToCardReference));
-		}
-	} else {
-		const dbReferences = await prisma.cardReference.findMany({
-			where: { cardId: card.id, reversed },
-		});
-		references.push(...dbReferences.map(dbToCardReference));
-	}
+	const dbReferences = await prisma.cardReference.findMany({
+		where: { card: card.name, reversed, userId: getCurrentUserId() },
+	});
+	const references = dbReferences.map(dbToCardReference);
 	if (isTarotCard(card)) {
 		references.push(getDefaultCardReference(card.name, reversed));
 	}
