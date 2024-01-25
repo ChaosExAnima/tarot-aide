7cd6d6dd89456413762f5db4e73e0638a13c9a76
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-25 17:45:15 -0500

    Refactor avatar API handler

--- pages/api/avatar.ts
+++ pages/api/avatar.ts
@@ -1,15 +1,14 @@
 import { createHash } from 'crypto';
 
-import { ResponseBody, handlerWithError, headersFromRequest } from 'lib/api';
-import { loadUserFromHeaders } from 'lib/users';
+import { ResponseBody, handlerWithError } from 'lib/api';
+import { userFromApiRequest } from 'lib/users';
 
 export interface AvatarResponse extends ResponseBody {
 	url: string;
 }
 
 const handler = handlerWithError<AvatarResponse>(['GET'], async (req) => {
-	const headers = headersFromRequest(req);
-	const user = await loadUserFromHeaders(headers);
+	const user = await userFromApiRequest(req);
 	const hash = createHash('sha256')
 		.update(user.email.trim().toLowerCase())
 		.digest('hex');
