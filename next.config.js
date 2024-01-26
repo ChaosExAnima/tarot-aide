25bdb83e5952c621c43966012ab3b520adb4338e
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-26 00:45:29 -0500

    Add .env.production and update nginx.conf and next.config.js

--- next.config.js
+++ next.config.js
@@ -8,11 +8,10 @@ const nextConfig = {
 		remotePatterns: [{ protocol: 'https', hostname: 'picsum.photos' }],
 	},
 };
-if (process.env.NODE_ENV === 'production') {
-	nextConfig.basePath = process.env.BASE_PATH ?? '';
-	nextConfig.assetPrefix = process.env.BASE_PATH ? `${process.env.BASE_PATH}/` : '';
+if (process.env.BASE_PATH) {
+	nextConfig.basePath = process.env.BASE_PATH;
+	nextConfig.assetPrefix = `${process.env.BASE_PATH}/`;
+	console.log('Using basePath of', nextConfig.basePath);
 }
-
-console.log('Building on basePath:', nextConfig.basePath);
 
 export default nextConfig;
