6bd0416471276e85170599524ee37ba57ad8931f
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-01-27 01:39:28 -0500

    Add TagPicker component

---
+++ components/tag-picker.tsx
@@ -0,0 +1,64 @@
+import { Chip, Input, InputProps } from '@nextui-org/react';
+import { useState, KeyboardEvent } from 'react';
+
+import { displayCase } from 'lib/text';
+
+interface TagPickerProps {
+	tags?: string[];
+	onChange: (tags: string[]) => void;
+}
+
+export default function TagPicker({
+	tags: initialTags = [],
+	onChange,
+	...props
+}: TagPickerProps & Omit<InputProps, 'onChange'>) {
+	const [tagInput, setTagInput] = useState('');
+	const [tags, setTags] = useState(initialTags);
+	const handleTags = (cb: (tags: string[]) => string[]) => {
+		setTags((tags) => {
+			const newTags = cb(tags);
+			onChange(newTags);
+			return newTags;
+		});
+	};
+
+	const handleKeywordChange = (tag: string) => {
+		if (tag.endsWith(',')) {
+			handleTags((tags) => [...tags, tag.slice(0, -1).trim()]);
+			setTagInput('');
+		} else {
+			setTagInput(tag);
+		}
+	};
+	const handleBackspace = (event: KeyboardEvent) => {
+		if (event.key === 'Backspace' && !tagInput) {
+			handleTags((tags) => tags.slice(0, -1));
+		}
+	};
+
+	const chips = tags.map((tag) => (
+		<Chip
+			key={tag}
+			size="sm"
+			onClose={() => handleTags((tags) => tags.filter((t) => t !== tag))}
+		>
+			{displayCase(tag)}
+		</Chip>
+	));
+
+	return (
+		<Input
+			startContent={
+				chips.length > 0 && (
+					<span className="flex flex-nowrap gap-1">{chips}</span>
+				)
+			}
+			onValueChange={handleKeywordChange}
+			value={tagInput}
+			onClear={() => handleTags(() => [])}
+			onKeyDown={handleBackspace}
+			{...props}
+		/>
+	);
+}
