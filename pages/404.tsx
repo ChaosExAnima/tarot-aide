be66a1f57e2a645bb652c1e9b966e93c7486cc73
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-07-04 23:56:43 +0200

    adds 404

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import Page from 'components/page';
+
+export default function Page404() {
+	return (
+		<Page title="Not found!">
+			<h1 className="text-6xl text-center mt-6">Nothing here!</h1>
+			<h2 className="text-2xl text-center mt-4">
+				Perhaps you should consult the Sun?
+			</h2>
+		</Page>
+	);
+}
