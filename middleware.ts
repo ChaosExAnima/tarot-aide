4b86dae43d563f10c537d3aa69d1a40e9074f30a
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-07-05 18:23:38 +0200

    adds csrf middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,35 @@
+import { verifyRequestOrigin } from 'lucia';
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+function handleImages(req: NextRequest) {
+	const url = req.nextUrl.clone();
+	url.pathname = `${process.env.BASE_PATH ?? ''}/api${url.pathname}`;
+	return NextResponse.rewrite(url);
+}
+
+export async function middleware(req: NextRequest) {
 	// Rewrite /images/ to /api/images/
-	if (req.nextUrl.pathname.startsWith('/images/')) {
-		const url = req.nextUrl.clone();
-		url.pathname = `${process.env.BASE_PATH ?? ''}/api${url.pathname}`;
-		return NextResponse.rewrite(url);
+	if (req.method === 'GET' && req.nextUrl.pathname.startsWith('/images/')) {
+		return handleImages(req);
 	}
-	if (process.env.NODE_ENV === 'production') {
-		// TODO: Actual logging.
-		console.log('request:', req.method, req.nextUrl.pathname);
+	// CSRF: https://lucia-auth.com/guides/validate-session-cookies/nextjs-pages
+	if (req.method !== 'GET') {
+		const originHeader = req.headers.get('Origin');
+		// NOTE: You may need to use `X-Forwarded-Host` instead
+		const hostHeader = req.headers.get('Host');
+		if (
+			!originHeader ||
+			!hostHeader ||
+			!verifyRequestOrigin(originHeader, [hostHeader])
+		) {
+			return new NextResponse(null, {
+				status: 403,
+			});
+		}
 	}
+	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+	matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
