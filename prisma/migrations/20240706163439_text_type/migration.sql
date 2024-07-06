5a05a77bcdd0d953330db3db08d03fb7615550e3
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-07-06 18:35:59 +0200

    updates schema to have longer text

---
+++ prisma/migrations/20240706163439_text_type/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `CardReference` MODIFY `text` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Position` MODIFY `notes` TEXT NULL;
+
+-- AlterTable
+ALTER TABLE `Spread` MODIFY `notes` TEXT NULL;
