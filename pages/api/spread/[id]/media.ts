cf1767beffb911f4b4eeabf9a00241e0971b4cc8
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-07-07 23:43:16 +0200

    cleans up media upload a bit

--- pages/api/spread/[id]/media.ts
+++ pages/api/spread/[id]/media.ts
@@ -33,42 +33,47 @@ const handler = handlerWithError(
 		if (req.method === 'DELETE') {
 			return handleDelete(spreadId, user.id, req);
 		}
-		const uploadDir = await ensureUploadPath(user.id);
-		const [fields, files] = await parseForm<'type', 'media'>(
-			req,
-			uploadDir,
-		);
-		try {
-			const type = fields.type?.at(0);
-			if (!type || !includes<MediaType>(['photo', 'audio'], type)) {
-				throw new ApiError(400, 'Missing or invalid type');
-			}
-			const media = files.media?.at(0);
-			if (!media) {
-				throw new ApiError(400, 'Missing photo or audio');
-			}
-			// Delete old media, if it exists
-			await deleteSpreadMedia(spreadId, type, user.id);
-			let newMedia: Media;
-			if (type === 'photo') {
-				newMedia = await processPhoto(media, spreadId, user.id);
-			} else {
-				throw new ApiError(501, 'Audio not implemented');
-			}
-			return {
-				success: true,
-				message: 'Media uploaded',
-				media: newMedia,
-			} satisfies SpreadMediaUploadResponse;
-		} catch (err) {
-			const media = files.media?.at(0);
-			if (media) {
-				await deleteMedia(media);
-			}
-			throw err;
-		}
+		return handleCreate(spreadId, user.id, req);
 	},
 );
+
+async function handleCreate(
+	spreadId: number,
+	userId: string,
+	req: NextApiRequest,
+): Promise<SpreadMediaUploadResponse> {
+	const uploadDir = await ensureUploadPath(userId);
+	const [fields, files] = await parseForm<'type', 'media'>(req, uploadDir);
+	try {
+		const type = fields.type?.at(0);
+		if (!type || !includes<MediaType>(['photo', 'audio'], type)) {
+			throw new ApiError(400, 'Missing or invalid type');
+		}
+		const media = files.media?.at(0);
+		if (!media) {
+			throw new ApiError(400, 'Missing photo or audio');
+		}
+		// Delete old media, if it exists
+		await deleteSpreadMedia(spreadId, type, userId);
+		let newMedia: Media;
+		if (type === 'photo') {
+			newMedia = await processPhoto(media, spreadId, userId);
+		} else {
+			throw new ApiError(501, 'Audio not implemented');
+		}
+		return {
+			success: true,
+			message: 'Media uploaded',
+			media: newMedia,
+		} satisfies SpreadMediaUploadResponse;
+	} catch (err) {
+		const media = files.media?.at(0);
+		if (media) {
+			await deleteMedia(media);
+		}
+		throw err;
+	}
+}
 
 async function handleDelete(
 	spreadId: number,
@@ -83,10 +88,7 @@ async function handleDelete(
 					includes<MediaType>(['photo', 'audio'], type),
 				),
 		})
-		.parse(req.body);
-	if (!type) {
-		throw new ApiError(400, 'Missing or invalid type');
-	}
+		.parse(req.query);
 	const results = await deleteSpreadMedia(spreadId, type, userId);
 	return {
 		success: true,
